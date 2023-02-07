3de33bc15d38af272d259534f16bd55835766b05
Adrianucho <lowerpm@gmail.com>
2023-02-07 14:23:48 +0100

    AC: Salto implementado
    - Se ha creado la programación que permite a Slime saltar.
    - Se ha creado un objeto vacío dentro de Slime que permite detectar todo aquello que tenga la Layer suelo.
    - Se han ajustado los parámetros del RigigBody de Slime para mejorar la experiencia de juego.

--- Assets/Programacion/Disparo Enemigo/Player.cs
+++ Assets/Programacion/Disparo Enemigo/Player.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    public float speed;
+    public float jump;
+
+    private float move;
+
+    public Rigidbody2D rb;
+
+
+    const float groundCheckRadius = 0.2F;
+
+    [SerializeField] Transform groundCheckCollider;
+    public bool isGrounded = false;
+    [SerializeField] LayerMask groundLayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +28,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        GroundCheck();
+        move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * move, rb.velocity.y);
+
+        if (Input.GetButtonDown("Jump") && isGrounded == true)
+        {
+            rb.AddForce(new Vector2(rb.velocity.x, jump));
+
+        }
+
     }
+
+    void GroundCheck()
+    {
+        isGrounded = false;
+
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position,groundCheckRadius, groundLayer);
+        if(colliders.Length > 0)
+        {
+            isGrounded = true;
+
+        }
+    }
+
 }
