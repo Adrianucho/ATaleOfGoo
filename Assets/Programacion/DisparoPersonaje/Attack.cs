b82e5f222c48ea25a3fd90c94b79a53cda19c5c9
DesignGrinch <adrip37@gmail.com>
2023-02-10 11:19:18 +0100

    AP: Disparo personaje mejorado
    - Se ha cambiado el disparo del personaje
    -Se ha implementado que con el ratón se pueda disparar

--- Assets/Programacion/DisparoPersonaje/Attack.cs
+++ Assets/Programacion/DisparoPersonaje/Attack.cs
@@ -7,25 +7,40 @@ public class Attack : MonoBehaviour
     public Transform firepoint; // referencia de adonde tiene que disparar 
     public GameObject goo;
 
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public bool canFire;
+    private float timer;
+    public float timeBetweenFiring;
 
     void Start()
     {
-        
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1")) //si pulsa el botón se ejecuta el disparo
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition); //sirve para que gracias a la camara pueda rotar para disparar
+
+        Vector3 rotation = mousePos - transform.position;
+        float rotZ = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rotZ);
+
+
+
+
+      if (Input.GetButtonDown("Fire1")) //si pulsa el botón se ejecuta el disparo
         {
             Shoot();
         }
 
-
+       
 
     }
     void Shoot ()
     {
         Instantiate(goo, firepoint.position, firepoint.rotation); //instancia la bala en donde le pide
     }
+  
 }
--- Assets/Programacion/DisparoPersonaje/Goo.cs
+++ Assets/Programacion/DisparoPersonaje/Goo.cs
@@ -7,9 +7,23 @@ public class Goo : MonoBehaviour
     public float speed = 20f;
     public Rigidbody2D rb; //referencia del rigid body
     public int damage = 20; //daño que hace el personaje
+
+    private Camera mainCam;
+    private Vector3 mousePos;
+    public float force;
+
     void Start()
     {
-        rb.velocity = transform.right * speed; // le dice adonde tiene que ir
+     /*   rb.velocity = transform.right * speed; // le dice adonde tiene que ir
+     */
+        mainCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        rb = GetComponent<Rigidbody2D>();
+        mousePos = mainCam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 direction = mousePos - transform.position;
+        Vector3 rotation = transform.position - mousePos;
+        rb.velocity = new Vector2(direction.x, direction.y).normalized * force;
+        float rot = Mathf.Atan2(rotation.y, rotation.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0, 0, rot + 90);
     }
 
     void OnTriggerEnter2D(Collider2D hitInfo) //si collisiona con el jugador se destruye
