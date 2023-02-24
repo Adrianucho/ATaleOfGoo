dc6e3e9a266f44bd7159e8b8396de6692c25b429
DesignGrinch <adrip37@gmail.com>
2023-02-24 10:46:34 +0100

    AP: BALA Y MENU

--- Assets/Programacion/Menú/DialogueTrigger.cs
+++ Assets/Programacion/Menú/DialogueTrigger.cs
@@ -10,4 +10,17 @@ public class DialogueTrigger : MonoBehaviour //script que triggea la conversació
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue); //busca al dialogue manager y hace que empieze la conver
     }
+
+    void OnTriggerEnter2D(Collider2D col) //si collisiona con el jugador se destruye
+    {
+        if (col.gameObject.tag == "Goo")
+        {
+            Debug.Log("Le ha dado");
+            //El juego comprueba si hacer daño o no
+            FindObjectOfType<DialogueManager>().StartDialogue(dialogue); //busca al dialogue manager y hace que empieze la conver
+
+         
+
+        }
+    }
 }
